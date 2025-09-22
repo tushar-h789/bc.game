0d07045fd74d66bd83ab2e002f64856324f76c27
tushar-h789 <tushar789imran@gmail.com>
2025-09-23 01:47:16 +0600

    added carusal

---
+++ app/(casino)/casino/_components/re-usable/game-card.tsx
@@ -0,0 +1,31 @@
+// GameCard.tsx
+import Image, { StaticImageData } from "next/image";
+import React from "react";
+
+interface GameItem {
+  image: string | StaticImageData;
+  title: string;
+  multiplier: string;
+  players: string;
+}
+
+interface GameCardProps {
+  gameData: GameItem;
+}
+
+const GameCard = ({ gameData }: GameCardProps) => {
+  return (
+    <div className="game-card">
+      <div className="relative w-full aspect-[4/5] overflow-hidden rounded-md">
+        <Image src={gameData.image} alt="game" fill sizes="(max-width: 768px) 50vw, 20vw" className="object-cover" />
+      </div>
+      <div className="game-card__details">
+        <h3 className="game-card__title">{gameData.title}</h3>
+        <p className="game-card__multiplier">{gameData.multiplier}x</p>
+        <p className="game-card__players">{gameData.players} Playing</p>
+      </div>
+    </div>
+  );
+};
+
+export default GameCard;
--- app/(casino)/casino/page.tsx
+++ app/(casino)/casino/page.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+'use client'
+import React, { useRef } from "react";
 import Banner from "./_components/re-usable/banner";
 import bannerImg from "@/public/images/common/banner-img.png";
+import GameCarousel, { GameCarouselHandle } from "./_components/re-usable/game-carosel";
+import gameCardImage from "@/public/images/common/game-card-img.png"
+
+const games = [
+  {
+    image: gameCardImage,
+    title: "LIMBO",
+    multiplier: "500",
+    players: "2,110",
+  },
+  {
+    image: gameCardImage,
+    title: "LIMBO",
+    multiplier: "500",
+    players: "2,110",
+  },
+  {
+    image: gameCardImage,
+    title: "LIMBO",
+    multiplier: "500",
+    players: "2,110",
+  },
+  {
+    image: gameCardImage,
+    title: "LIMBO",
+    multiplier: "500",
+    players: "2,110",
+  },
+];
 
 // This is the Home tab page: /casino
 export default function CasinoHome() {
+  const carouselRef = useRef<GameCarouselHandle | null>(null);
   return (
     <div>
       <div className="py-4 bg-gray-900 flex items-center justify-center">
@@ -13,6 +44,18 @@ export default function CasinoHome() {
           buttonText="Join Now"
           imageSrc={bannerImg}
         />
+      </div>
+
+      {/* Hot game carosel */}
+      <div className="app-container">
+        <div className="flex items-center justify-between mb-2">
+          <h2>Hot Games</h2>
+          <div className="flex items-center gap-2">
+            <button onClick={() => carouselRef.current?.prev()} className="carousel-button" aria-label="Previous">‹</button>
+            <button onClick={() => carouselRef.current?.next()} className="carousel-button" aria-label="Next">›</button>
+          </div>
+        </div>
+        <GameCarousel ref={carouselRef} games={games} />
       </div>
     </div>
   );

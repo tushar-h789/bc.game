d2b834520cd4563b899a7ec4c214c86f31638c91
tushar-h789 <tushar789imran@gmail.com>
2025-09-27 00:45:45 +0600

    updated

--- app/(casino)/casino/sport/page.tsx
+++ app/(casino)/casino/sport/page.tsx
@@ -2,9 +2,12 @@
 import teamAFlag from "@/public/images/home/india.png";
 import teamBFlag from "@/public/images/home/bangladesh.png";
 import tournamentLogo from "@/public/images/home/asio-cup-logo.png";
-import ReUsableSportsSlider, { type MatchSlideData } from "../_components/re-usable/re-usable-sports-slider";
+import ReUsableSportsSlider, {
+  type MatchSlideData,
+} from "../_components/re-usable/re-usable-sports-slider";
 import Banner from "../_components/re-usable/banner";
-import PreMatch from "./_components/pre-match"
+import PreMatch from "./_components/pre-match";
+import bannerImg from "@/public/images/common/banner-img.png";
 
 const matches: MatchSlideData[] = [
   {
@@ -38,48 +41,49 @@ const matches: MatchSlideData[] = [
 
 export default function Page() {
   return (
-   <div>
     <div>
-      <Banner />
-    </div>
-    <div className=" p-4">
-          <div className="relative w-full max-w-sm">
-            <span className="absolute inset-y-0 left-3 flex items-center text-gray-400">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1110.5 3a7.5 7.5 0 016.15 13.65z"
-                />
-              </svg>
-            </span>
+      <div className="flex items-center justify-center mx-2">
+        <Banner
+          title="Stay Untamed"
+          subTitle="Registration Now & Unlock"
+          description=" Up To $10,000 in Casino or Sports!"
+          buttonText="Join Now"
+          imageSrc={bannerImg}
+        />
+      </div>
+      <div className=" p-4">
+        <div className="relative w-full max-w-sm">
+          <span className="absolute inset-y-0 left-3 flex items-center text-gray-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1110.5 3a7.5 7.5 0 016.15 13.65z"
+              />
+            </svg>
+          </span>
 
-            <input
-              type="text"
-              placeholder="Search Games"
-              className="w-full rounded-md bg-[#213744] pl-10 pr-4 py-2 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+          <input
+            type="text"
+            placeholder="Search Games"
+            className="w-full rounded-md bg-[#213744] pl-10 pr-4 py-2 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
         </div>
+      </div>
 
-     <div className="p-1">
-      <ReUsableSportsSlider
-        title="Upcoming Matches" 
-        slides={matches} 
-      />
+      <div className="p-1">
+        <ReUsableSportsSlider title="Upcoming Matches" slides={matches} />
+      </div>
+      <div>
+        <PreMatch title="Top pre-match" slides={matches} />
+      </div>
     </div>
-    <div>
-    <PreMatch
-      title="Top pre-match" 
-      slides={matches}  />
-    </div>
-   </div>
   );
 }
